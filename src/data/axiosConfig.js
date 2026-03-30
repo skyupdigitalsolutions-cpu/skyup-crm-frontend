eae2385507b2a773b646681045ecd34945b67ca0
sjsjasss2001@gmail.com <sjsjasss2001@gmail.com>
2026-03-30 14:08:45 +0530

    updated backend URL

--- src/data/axiosConfig.js
+++ src/data/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://skyup-crm-backend.onrender.com/api",
 });
 
 // Automatically attach token to every request
