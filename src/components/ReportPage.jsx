68016133d17c9565b59698f4c4c10192d2635448
Pooja08-kad <poojakadwadi8888@gmail.com>
2026-04-25 14:26:24 +0530

    updated the reportPages

--- src/components/ReportPage.jsx
+++ src/components/ReportPage.jsx
@@ -736,7 +736,7 @@ export default function ReportPage() {
             <>
               <input ref={importInputRef} type="file" accept=".csv" className="hidden" onChange={handleImportCSV} />
               <div className="flex items-center rounded-xl overflow-hidden border border-[#7C3AED] text-[#7C3AED]">
-                <button onClick={() => importInputRef.current?.click()} className="flex items-center gap-2 px-4 py-2 bg-[#7C3AED] text-white text-[13px] font-semibold hover:bg-violet-700 transition border-r border-violet-500">
+                <button onClick={() => importInputRef.current?.click()} className="flex items-center gap-2  py-2 bg-[#7C3AED] text-white text-[13px] font-semibold hover:bg-violet-700 transition border-r border-violet-500">
                   <svg className="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}><path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"/></svg>
                   Import CSV
                 </button>
