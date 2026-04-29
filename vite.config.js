03ebaf9dc6b6509aca8402f4e59b26905748ac8b
srinivasjsutar <sjsjasss2001@gmail.com>
2026-04-29 12:50:21 +0530

    updated the issue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
   appType: 'spa',   // ← tells Vite's preview server to serve index.html for all 404s
   server: {
     host: true,
