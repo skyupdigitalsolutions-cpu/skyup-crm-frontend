6652db06c964ae278303b731c00f8b7f01a968ec
Pooja <poojakadwadi8888@gmail.com>
2026-05-03 15:35:39 +0530

    Add 'Call Recordings' link to Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -356,4 +356,13 @@ export function Sidebar() {
       </div>
     </>
   );
+    {
+      to: "/call-recordings",
+      label: "Call Recordings",
+      icon: (
+        <svg width="18" height="18" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1.8}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"/>
+        </svg>
+      ),
+    },
 }
