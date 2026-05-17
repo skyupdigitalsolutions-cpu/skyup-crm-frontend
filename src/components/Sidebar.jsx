6159cb0c7bf80147cd0edc11730331a6299f48bb
Pooja <poojakadwadi8888@gmail.com>
2026-05-17 14:33:26 +0530

    Remove 'Call Recordings' from Sidebar component
    Removed 'Call Recordings' menu item from Sidebar.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -88,15 +88,7 @@ const ADMIN_NAV_ITEMS = [
       </svg>
     ),
   },
-  {
-    to: "/call-recordings",
-    label: "Call Recordings",
-    icon: (
-      <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="w-5 h-5">
-        <path d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"/>
-      </svg>
-    ),
-  },
+  
   {
     to: "/upgrade-plan",
     label: "Upgrade Plan",
