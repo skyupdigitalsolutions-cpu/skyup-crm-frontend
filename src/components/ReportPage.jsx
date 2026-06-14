155ea750e6f44b2e8dbd80d5a1c40edec215e233
Pooja <poojakadwadi8888@gmail.com>
2026-06-14 12:42:47 +0530

    Update ReportPage.jsx

--- src/components/ReportPage.jsx
+++ src/components/ReportPage.jsx
@@ -117,7 +117,7 @@ function AgentSelect({ value, onChange, agents, className }) {
       </button>
 
       {open && (
-        <div className="absolute z-50 top-full mt-1.5 left-0 w-56 bg-white dark:bg-[#1A1D27] border border-[#E4E7EF] dark:border-[#262A38] rounded-xl shadow-lg overflow-hidden">
+        <div className="absolute z-50 top-full mt-1.5 right-0 w-56 max-w-[calc(100vw-2rem)] bg-white dark:bg-[#1A1D27] border border-[#E4E7EF] dark:border-[#262A38] rounded-xl shadow-lg overflow-hidden">
           <div className="p-2 border-b border-[#E4E7EF] dark:border-[#262A38]">
             <div className="relative">
               <svg className="absolute left-2.5 top-1/2 -translate-y-1/2 w-3.5 h-3.5 text-[#8B92A9] pointer-events-none" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
